b10138905e68541c1e16aa7e17089cc5bde26fb0
benomar15 <61231576+kayaki15@users.noreply.github.com>
2021-10-07 14:31:19 +0100

    refactor code
    07/10/2021

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -29,21 +29,20 @@ public class UserController {
 
 	@GetMapping("/users")
 	public String listFirstPage(Model model) {
-		return listByPage(1, model, "firstName", "asc" , null);
+		return listByPage(1, model, "firstName", "asc", null);
 	}
 
 	@GetMapping("/users/page/{pageNum}")
 	public String listByPage(
-			
-			@PathVariable(name = "pageNum") int pageNum, Model model,
-			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
-			@Param("keyword") String keyword
-			
-			) {
+
+			@PathVariable(name = "pageNum") int pageNum, Model model, @Param("sortField") String sortField,
+			@Param("sortDir") String sortDir, @Param("keyword") String keyword
+
+	) {
 		System.out.println("Sort Field " + sortField);
 		System.out.println("Sort Order " + sortDir);
 
-		Page<User> page = service.listByPage(pageNum, sortField, sortDir , keyword);
+		Page<User> page = service.listByPage(pageNum, sortField, sortDir, keyword);
 
 		List<User> listusers = page.getContent();
 
@@ -115,9 +114,13 @@ public class UserController {
 		///
 
 		redirectAttributes.addFlashAttribute("message", "The User has been saved successfully.");
+		return getRedirectURLtoAffectedUser(user);
 
-		return "redirect:/users";
+	}
 
+	private String getRedirectURLtoAffectedUser(User user) {
+		String firstPartOfEmail = user.getEmail().split("@")[0];
+		return "redirect:/users/page/1/?sortField=id&sortDir=asc&keyword="  + firstPartOfEmail;
 	}
 
 	@GetMapping("/users/edit/{id}")
